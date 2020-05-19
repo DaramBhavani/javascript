3e81af62877fbaa26baf14a01e8b3721404437ed
darambhavani <daram.bhavani@gmail.com>
2020-05-20 00:32:23 +0800

    studied arrowfunctions and arrow functions implicit return

--- functions/ArrayCallbackMethods/arrowfunctions.js
+++ functions/ArrayCallbackMethods/arrowfunctions.js
@@ -1,8 +1,11 @@
-
+(function(){
 //Function Expression
 // const result = function(x){
 //     return x*x*x;
 // }
+
+
+//Arrow Functions
 
 //Example 1
 
@@ -27,3 +30,29 @@ const multiplication = (x,y) =>{
 const Greet = () =>{  
     console.log("hi");
 }
+
+//Arrow Function Implicit Return
+
+
+//Example 1
+
+const res = (n) => (    //const res = n => n*n;
+   n*n
+)
+
+//Example 2
+
+const sequenceNums=[20,5,90,10,7,40,15,13];
+
+const doubleNo = sequenceNums.map( n => n * 2 );
+
+//Example 3
+
+const printEvenOdd = sequenceNums.map((n) => (  //const printEvenOdd = sequenceNums.map( n => n%2===0 ? 'even' : 'odd');
+    n%2 === 0 ? 'even' : 'odd'
+));
+
+
+
+
+})();
